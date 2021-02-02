601f5680f4f4f8270b0763ee2f438390360b3562
Julien GAINZA <gainza.julien@gmail.com>
2021-02-02 11:32:41 +0100

    ajout pagination à l'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,13 +14,18 @@ class AccueilController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index( ProduitRepository $produitRepository): Response
+    public function index(ProduitRepository $produitRepository, Request $request): Response
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $produitRepository->getProduitPaginator($offset);
         
-        $produits = $produitRepository->findAll();
-
         return $this->render('accueil/index.html.twig', [
-            'produits' => $produits,
+            'produits' => $paginator,
+            'previous' => $offset - ProduitRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + ProduitRepository::PAGINATOR_PER_PAGE),
         ]);
     }
+
+
+
 }
