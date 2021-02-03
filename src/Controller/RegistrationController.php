dd6131ac37d15cf85a6a5be296c7b984c009bf4d
Julien GAINZA <gainza.julien@gmail.com>
2021-02-03 17:23:48 +0100

    changement formulaire register + ajout Bootstrap theme

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -42,7 +42,7 @@ class RegistrationController extends AbstractController
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
-                    $form->get('plainPassword')->getData()
+                    $form->get('password')->getData()
                 )
             );
             $user->setRoles(["ROLE_USER"]);
