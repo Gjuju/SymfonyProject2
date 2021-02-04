46104fd77f6effc0986de68baf4babbddea10854
Julien GAINZA <gainza.julien@gmail.com>
2021-02-04 14:35:49 +0100

    ajout routes administration User/ administrateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,22 +8,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+
     /**
-     * @Route("/logadmin", name="logadmin")
+     * @Route("/commandes_admin", name="commandes_admin")
      */
-    public function logAdmin(): Response
+    public function commandesAdmin(): Response
     {
-        return $this->render('logadmin/logAdmin.html.twig', [
+        return $this->render('admin/commandes_admin.html.twig', [
             
         ]);
     }
 
     /**
-     * @Route("/user", name="loguser")
+     * @Route("/produits_admin", name="produits_admin")
      */
-    public function logUser (): Response
+    public function produitAdmin(): Response
     {
-        return $this->render('user/logUser.html.twig', [
+        return $this->render('admin/produits_admin.html.twig', [
+            
+        ]);
+    }
+
+
+    /**
+     * @Route("/commandes_user", name="commandes_user")
+     */
+    public function commandesUser (): Response
+    {
+        return $this->render('admin/commandes_user.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/compte_user", name="compte_user")
+     */
+    public function compteUser (): Response
+    {
+        return $this->render('admin/compte_user.html.twig', [
 
         ]);
     }
