6a9a9d615f4962cef3379a241cf538b79dae9151
Julien GAINZA <gainza.julien@gmail.com>
2021-02-04 09:17:58 +0100

    register form + register twig

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,7 +24,11 @@ class RegistrationFormType extends AbstractType
             ->add('password')
             /* ->add('isVerified') */
             /* ->add('panier') */
-        ;
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [ 'class' => 'btn-primary' ]
+            ]);
+        
     }
 
     public function configureOptions(OptionsResolver $resolver)
