fae7b2c8966284452ac21b749790a249139ea455
Julien GAINZA <gainza.julien@gmail.com>
2021-02-04 13:20:58 +0100

    Mod 3 roles utilisateurs distincs "" "ROLE_USER" "ROLE_ADMIN"

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -117,7 +117,7 @@ class Utilisateur implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        // $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
